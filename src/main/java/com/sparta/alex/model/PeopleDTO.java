fe422df2b083b48f1bb07a0dace894d192760f41
Alex Ng <alexkng97@gmail.com>
2021-01-13 15:28:20 +0000

    getters and setters

--- src/main/java/com/sparta/alex/model/PeopleDTO.java
+++ src/main/java/com/sparta/alex/model/PeopleDTO.java
@@ -23,4 +23,132 @@ public class PeopleDTO {
 
 	public PeopleDTO() {
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getHeight() {
+		return height;
+	}
+
+	public void setHeight(String height) {
+		this.height = height;
+	}
+
+	public String getMass() {
+		return mass;
+	}
+
+	public void setMass(String mass) {
+		this.mass = mass;
+	}
+
+	public String getHair_color() {
+		return hair_color;
+	}
+
+	public void setHair_color(String hair_color) {
+		this.hair_color = hair_color;
+	}
+
+	public String getSkin_color() {
+		return skin_color;
+	}
+
+	public void setSkin_color(String skin_color) {
+		this.skin_color = skin_color;
+	}
+
+	public String getEye_color() {
+		return eye_color;
+	}
+
+	public void setEye_color(String eye_color) {
+		this.eye_color = eye_color;
+	}
+
+	public String getBirth_year() {
+		return birth_year;
+	}
+
+	public void setBirth_year(String birth_year) {
+		this.birth_year = birth_year;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public PlanetsDTO getHomeworld() {
+		return homeworld;
+	}
+
+	public void setHomeworld(PlanetsDTO homeworld) {
+		this.homeworld = homeworld;
+	}
+
+	public List<FilmsDTO> getFilms() {
+		return films;
+	}
+
+	public void setFilms(List<FilmsDTO> films) {
+		this.films = films;
+	}
+
+	public List<SpeciesDTO> getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(List<SpeciesDTO> species) {
+		this.species = species;
+	}
+
+	public List<VehiclesDTO> getVehicles() {
+		return vehicles;
+	}
+
+	public void setVehicles(List<VehiclesDTO> vehicles) {
+		this.vehicles = vehicles;
+	}
+
+	public List<StarshipsDTO> getStarships() {
+		return starships;
+	}
+
+	public void setStarships(List<StarshipsDTO> starships) {
+		this.starships = starships;
+	}
+
+	public String getCreated() {
+		return created;
+	}
+
+	public void setCreated(String created) {
+		this.created = created;
+	}
+
+	public String getEdited() {
+		return edited;
+	}
+
+	public void setEdited(String edited) {
+		this.edited = edited;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
 }
