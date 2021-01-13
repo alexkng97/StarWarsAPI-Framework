c78705edf8ba1f2541c70686c9675c3c9a12a67f
Alex Ng <alexkng97@gmail.com>
2021-01-13 14:18:27 +0000

    methods implemented in connection manager and tested

--- src/main/java/com/sparta/alex/controller/ConnectionManager.java
+++ src/main/java/com/sparta/alex/controller/ConnectionManager.java
@@ -3,8 +3,11 @@ package com.sparta.alex.controller;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
 
 public class ConnectionManager {
 
@@ -12,12 +15,18 @@ public class ConnectionManager {
 
 	HttpResponse httpResponse = null;
 	HttpClient httpClient = HttpClient.newHttpClient();
-
-	HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(BASE_URL)).build();
+	HttpRequest httpRequest;
+	HttpHeaders httpHeaders = null;
+	private Map<String, List<String>> headersMap;
 
 	public ConnectionManager(){
 		try{
+			httpRequest = HttpRequest.newBuilder().uri(URI.create(BASE_URL)).build();
 			httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+			httpHeaders = httpResponse.headers();
+
+			headersMap = httpHeaders.map();
+
 		}catch(InterruptedException | IOException e){
 			e.printStackTrace();
 		}
@@ -27,6 +36,30 @@ public class ConnectionManager {
 	public int getStatusCode(){
 		return httpResponse.statusCode();
 	}
+
+	public Map<String, List<String>> getResponseHeaders(){
+		return headersMap;
+	}
+
+	public void printAllResponseHeaders(){
+		headersMap.entrySet().stream().forEach(e -> System.out.println(e.getKey() + " " + e.getValue()));
+	}
+
+	public String getResponseServer(){
+		return searchResponseHeaders("server");
+	}
+
+	public String getResponseDate(){
+		return searchResponseHeaders("date");
+	}
+
+	public String searchResponseHeaders(String header){
+		header= header.toLowerCase();
+		List<String> allValues = httpHeaders.allValues(header);
+		return allValues.toString().replace('[',' ').replace(']', ' ').trim();
+
+	}
+
 
 
 
