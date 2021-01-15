d2bdd16e36380dd5176484cef4e1ef35c57d21ea
Alex Ng <alexkng97@gmail.com>
2021-01-15 10:02:54 +0000

    more tests for Injector

--- src/test/java/com/sparta/alex/model/InjectorTest.java
+++ src/test/java/com/sparta/alex/model/InjectorTest.java
@@ -13,4 +13,19 @@ public class InjectorTest {
 		PeopleDTO luke = (PeopleDTO) Injector.injectByURL("https://swapi.dev/api/people/1/");
 		Assertions.assertEquals("Luke Skywalker", luke.getName());
 	}
+
+	@Test
+	@DisplayName("Inject by url Planets")
+	void injectByUrlPlanets() {
+		PlanetsDTO planet = (PlanetsDTO) Injector.injectByURL("https://swapi.dev/api/planets/1/");
+		Assertions.assertEquals("Tatooine", planet.getName());
+
+	}
+
+	@Test
+	@DisplayName("inject by url films")
+	void injectByUrlFilms() {
+		FilmsDTO film = (FilmsDTO) Injector.injectByURL("https://swapi.dev/api/films/2/");
+		Assertions.assertEquals("The Empire Strikes Back" , film.getTitle());
+	}
 }
